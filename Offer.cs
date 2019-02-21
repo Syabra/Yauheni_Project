64f37c228b216d385f65a4d67f511e8cc06f6fb7
Yauheny <Syabra.Bel@gmail.com>
2019-02-20 17:12:27 -0900

    +add base file

---
+++ Offer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KvitkouNet.Logic.Common.Models.User;
+
+namespace KvitkouNet.Logic.Common.Models.Statistics
+{
+    class Offer
+    {
+        //provides the data of person who dealt an offer
+        public User.User OfferingPerson { get; set; }
+
+        //shows the name of offer
+        public string Name { get; set; }
+
+        //shows a date of offer
+        public DateTime? Date { get; set; }
+    }
+}
---
+++ Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvitkouNet.Logic.Common.Models.Statistics
+{
+    public interface IPoint
+    {
+
+    }
+
+    class Rating
+    {
+        //shows total ponts gained by whole time
+        public List<IPoint> Points { get; set; }
+
+        //shows a number of place in the board
+        public int Place { get; }
+
+        //shows total offers, dealing by this person
+        public List<Offer> Offers { get; }
+    }
+}
